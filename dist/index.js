f451c7ca61842c33e5692b9828123c4a65f0f3b8
LKD70 <lkdornan70@gmail.com>
2020-03-17 16:36:44 +0000

    Updated to use bing json data

--- dist/index.js
+++ dist/index.js
@@ -1,53 +1,13 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 const https_1 = require("https");
-const $ = require("cheerio");
-const formatNum = (number, float = false) => (float ? parseFloat : parseInt)(number.replace(/,/g, '')
-    .replace(/\-/g, '').replace(/\+/g, '')) || 0;
-const formatName = (name) => {
-    name = name.replace(/:/g, '');
-    if (name[0] === ' ')
-        name = name.substr(1);
-    if (name[name.length - 1] === ' ')
-        name = name.slice(0, -1);
-    return name;
-};
 const get = new Promise((resolve, reject) => {
-    const req = https_1.request('https://www.worldometers.info/coronavirus/', res => {
+    const req = https_1.request('https://bing.com/covid/data', res => {
         let da = '';
         res.on('data', d => da += d);
         req.on('error', reject);
         res.on('end', () => {
-            let data = { countries: {} };
-            const body = $.load(da);
-            body('#main_table_countries > tbody > tr').each((_ri, row) => {
-                const cols = $(row).find('td');
-                const name = formatName($(cols[0]).text());
-                if (name === 'Total') {
-                    data.total = {
-                        total_cases: formatNum($(cols[1]).text()),
-                        new_cases: formatNum($(cols[2]).text()),
-                        total_deaths: formatNum($(cols[3]).text()),
-                        new_deaths: formatNum($(cols[4]).text()),
-                        total_recovered: formatNum($(cols[5]).text()),
-                        active_cases: formatNum($(cols[6]).text()),
-                        serious_cases: formatNum($(cols[7]).text()),
-                        cases_per_million: formatNum($(cols[8]).text(), true)
-                    };
-                }
-                else {
-                    data.countries[name] = {
-                        total_cases: formatNum($(cols[1]).text()),
-                        new_cases: formatNum($(cols[2]).text()),
-                        total_deaths: formatNum($(cols[3]).text()),
-                        new_deaths: formatNum($(cols[4]).text()),
-                        total_recovered: formatNum($(cols[5]).text()),
-                        active_cases: formatNum($(cols[6]).text()),
-                        serious_cases: formatNum($(cols[7]).text()),
-                        cases_per_million: formatNum($(cols[8]).text(), true)
-                    };
-                }
-            });
+            const data = JSON.parse(da);
             resolve(data);
         });
     });
--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,29 @@
 'use strict';
 
 import { request } from 'https';
-import * as $ from 'cheerio';
 
-interface Icountry {
-    total_cases: number,
-    new_cases: number,
-    total_deaths: number,
-    new_deaths: number,
-    total_recovered: number,
-    active_cases: number,
-    serious_cases: number,
-    cases_per_million: number,
+interface IArea {
+    totalConfirmed: number,
+    totalDeaths: number,
+    totalRecovered: number,
+    id: string,
+    lastUpdated: string,
+    areas: [IArea],
+    displayName: string,
+    lat: number,
+    long: number,
+    country: string,
+    parentId: string,
 }
 
-interface Idata {
-    countries?: {
-        [name: string]: Icountry
-    },
-    total?: Icountry
-}
-
-const formatNum = (number: string, float: boolean = false) =>
-    (float ? parseFloat : parseInt)(number.replace(/,/g, '')
-        .replace(/\-/g, '').replace(/\+/g, '')) || 0;
-
-const formatName = (name: string) => {
-    name = name.replace(/:/g, '');
-    if (name[0] === ' ') name = name.substr(1);
-    if (name[name.length - 1] === ' ') name = name.slice(0, -1);
-    return name;
-}
-
-const get = new Promise<Idata>((resolve, reject) => {
-    const req = request('https://www.worldometers.info/coronavirus/', res => {
+const get = new Promise<IArea>((resolve, reject) => {
+    const req = request('https://bing.com/covid/data', res => {
         let da = '';
         res.on('data', d => da += d);
         req.on('error', reject);
 
         res.on('end', () => {
-            let data: Idata = { countries: {} };
-            const body = $.load(da);
-
-            body('#main_table_countries > tbody > tr').each((_ri, row) => {
-                const cols = $(row).find('td');
-                const name = formatName($(cols[0]).text());
-                if (name === 'Total') {
-                    data.total = {
-                        total_cases: formatNum($(cols[1]).text()),
-                        new_cases: formatNum($(cols[2]).text()),
-                        total_deaths: formatNum($(cols[3]).text()),
-                        new_deaths: formatNum($(cols[4]).text()),
-                        total_recovered: formatNum($(cols[5]).text()),
-                        active_cases: formatNum($(cols[6]).text()),
-                        serious_cases: formatNum($(cols[7]).text()),
-                        cases_per_million: formatNum($(cols[8]).text(), true)
-                    }
-                } else {
-                    data.countries[name] = {
-                        total_cases: formatNum($(cols[1]).text()),
-                        new_cases: formatNum($(cols[2]).text()),
-                        total_deaths: formatNum($(cols[3]).text()),
-                        new_deaths: formatNum($(cols[4]).text()),
-                        total_recovered: formatNum($(cols[5]).text()),
-                        active_cases: formatNum($(cols[6]).text()),
-                        serious_cases: formatNum($(cols[7]).text()),
-                        cases_per_million: formatNum($(cols[8]).text(), true)
-                    };
-                }
-            });
-
+            const data = JSON.parse(da);
             resolve(data);
         });
     });
